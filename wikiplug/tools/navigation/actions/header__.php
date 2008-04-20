c439054a478ecf64d804abb16fe98b7d6e56c0a5
ddelon <ddelon>
2008-04-20 17:20:27 +0000

    Suppression banniere

--- wikiplug/tools/navigation/actions/header__.php
+++ wikiplug/tools/navigation/actions/header__.php
@@ -18,7 +18,6 @@ if (version_compare(phpversion(), '5.0') < 0) {
 
 
 $menu_page=$this->config["menu_page"];
-$banniere_page=$this->config["banniere_page"];
 if (isset($menu_page) and ($menu_page!=""))
     {
     // Ajout Menu de Navigation
@@ -34,20 +33,6 @@ if (isset($menu_page) and ($menu_page!=""))
     echo '<td class="body_column">';
  }
  
-if (isset($banniere_page) and ($banniere_page!=""))
-    {
-    // Ajout Banniere de Navigation
-    echo '<div id="banniere">';
-    $wikiBanniere = clone($this);
-    $wikiBanniere->tag=$banniere_page;
-
-
-    $wikiBanniere->SetPage($wikiBanniere->LoadPage($wikiBanniere->tag));
-    echo $wikiBanniere->Format($wikiBanniere->page["body"], "wakka");
-    echo '</div>';
-}
-
-
  
    	$plugin_output_new=preg_replace ('/<head>/',
 	'
@@ -56,8 +41,11 @@ if (isset($banniere_page) and ($banniere_page!=""))
 	.page_table {margin: 0px; padding: 0px ; border: none; height: 100%;width: 100%;} 
 	.menu_column {background-color: #FFFFCC; vertical-align: top; width: 150px; border: 1px solid #000000;padding:5px;}
 	.body_column {vertical-align: top; border: none;padding:5px;}
-	 #banniere {background-color: #FFFFCC; height: 104px;}
 	</style>
 	',
 	$plugin_output_new);
+	
+/**
+ * $log:$
+ */
 ?>
