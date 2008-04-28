05c6b21562b635c01cb6ec44593cec9b149b2293
mrflos <mrflos>
2008-04-28 13:51:28 +0000

    meilleure gestion des dates, simplication des templates

--- wikiplug/tools/syndication/actions/syndication.php
+++ wikiplug/tools/syndication/actions/syndication.php
@@ -19,9 +19,6 @@ if (file_exists('cache')) {
 $titre = $this->GetParameter("titre");
 
 $nb = $this->GetParameter("nb");
-if (empty($nb)) {
-	$nb=0;
-}
 
 $nouvellefenetre = $this->GetParameter("nouvellefenetre");
 
@@ -41,64 +38,64 @@ if (empty($template)) {
 //recuperation du parametre obligatoire des urls
 $urls = $this->GetParameter("url");
 if (!empty($urls)) {
-	$tab_url = array_map('trim', explode(',', $urls));
+	$tab_url = array_map('trim', explode(',', $urls));	
     foreach ($tab_url as $cle => $url) {    		
-			if ($url != '') {
-				$aso_site = array();
+			if ($url != '') {				
 				// Liste des encodages acceptes pour les flux
 				$encodages = 'UTF-8, ISO-8859-1, ISO-8859-15';
-				
+				// On parse l'url avec magpie
 				$feed = fetch_rss( $url );
-				if ($feed) {
-					// Gestion du titre
-					if ( $titre == '' ) {
-						$aso_site['titre'] = mb_convert_encoding($feed->channel['title'], 'HTML-ENTITIES', $encodages);
-					} else {
-						$aso_site['titre'] = $titre;
-					}
-					// Gestion de l'url du site
-					$aso_site['url'] = htmlentities($feed->channel['link']);
-	
-					// Ouverture du lien dans une nouvelle fenetre
-					$aso_site['ext'] = $nouvellefenetre;
-					
-					// Gestion des pages syndiquees
+				if ($feed) {									
+					// Gestion du nombre de pages syndiquees
 					$i = 0;
 				    $nb_item = count($feed->items);
 					foreach ($feed->items as $item) {					
 						if ($nb != 0 && $nb_item >= $nb && $i >= $nb) {
 							break;
-						}
+						}						
 						$i++;
 						$aso_page = array();
-						$aso_page['site'] = $aso_site;	
+						// Gestion du titre
+						if ( $titre == '' ) {
+							$aso_page['titre_site'] = mb_convert_encoding($feed->channel['title'], 'HTML-ENTITIES', $encodages);
+						} else {
+							$aso_page['titre_site'] = $titre;
+						}
+						// Gestion de l'url du site
+						$aso_page['url_site'] = htmlentities($feed->channel['link']);
+						// Ouverture du lien dans une nouvelle fenetre
+						$aso_page['ext'] = $nouvellefenetre;
+						//url de l'article	
 						$aso_page['url'] = htmlentities($item['link']);
+						//titre de l'article
 						$aso_page['titre'] = mb_convert_encoding($item['title'], 'HTML-ENTITIES', $encodages);
-						$aso_page['description'] = mb_convert_encoding($item['description'], 'HTML-ENTITIES', $encodages);
-						if (is_string($item['pubdate'])) $aso_page['date']=strtotime($item['pubdate']);
-						else $aso_page['date'] = $item['pubdate'];
+						//description de l'article
+						$aso_page['description'] = mb_convert_encoding($item['description'], 'HTML-ENTITIES', $encodages);					
+						//gestion de la date de publication, selon le flux, elle se trouve parsee à des endroits differents 
+						if ($item['pubdate']) $aso_page['datestamp'] = strtotime($item['pubdate']);
+       					elseif ($item['dc']['date']) $aso_page['datestamp'] = parse_w3cdtf($item['dc']['date']);
+        				elseif ($item['issued']) $aso_page['datestamp'] = parse_w3cdtf($item['issued']);
+        				else $aso_page['datestamp'] = time();						
 						if ($formatdate!='') {
 							switch ($formatdate) {							
 								case 'jm' :
-									$aso_page['date'] = strftime('%d.%m', $aso_page['date']);
+									$aso_page['date'] = strftime('%d.%m', $aso_page['datestamp']);
 									break;
 								case 'jma' :
-									$aso_page['date'] = strftime('%d.%m.%Y', $aso_page['date']);
+									$aso_page['date'] = strftime('%d.%m.%Y', $aso_page['datestamp']);
 									break;
 								case 'jmh' :
-									$aso_page['date'] = strftime('%d.%m %H:%M', $aso_page['date']);
+									$aso_page['date'] = strftime('%d.%m %H:%M', $aso_page['datestamp']);
 									break;
 								case 'jmah' :
-									$aso_page['date'] = strftime('%d.%m.%Y %H:%M', $aso_page['date']);
+									$aso_page['date'] = strftime('%d.%m.%Y %H:%M', $aso_page['datestamp']);
 									break;
 								default :
 									$aso_page['date'] = '';
 							}
-						}
-						$aso_site['pages'][] = $aso_page;
-						$syndication['pages'][strtotime($aso_page['date'])] = $aso_page;
+						}						
+						$syndication['pages'][$aso_page['datestamp']] = $aso_page;
 					}
-					$syndication['sites'][] = $aso_site;
 				} else {
 					echo '<p class="erreur">Erreur '.magpie_error().'</p>'."\n";        			    
 				}			
@@ -106,11 +103,9 @@ if (!empty($urls)) {
         }    
 	// Trie des pages par date
 	krsort($syndication['pages']);
-	
-	//+----------------------------------------------------------------------------------------------------------------+
-    // Extrait les variables et les ajoutes a l'espace de noms local
+
+	if (count($tab_url)==1 || $titre!='') echo '<h2 class="rss_site_titre"><a href="'.$syndication['pages'][key($syndication['pages'])]['url_site'].'">'.$syndication['pages'][key($syndication['pages'])]['titre_site'].'</a></h2>'."\n";
 	// Gestion des squelettes
-	extract($syndication);
 	include($template);
 } else {
 	echo 'Il faut entrer obligatoirement le param&ecirc;tre de l\'url pour syndiquer un flux RSS.';
