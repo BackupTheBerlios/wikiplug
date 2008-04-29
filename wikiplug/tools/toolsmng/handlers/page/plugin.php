3841a683f597290eec9b5426b34893cccd9397cf
ddelon <ddelon>
2008-04-29 08:13:10 +0000

    Liste des plugins actifs seulement

--- wikiplug/tools/toolsmng/handlers/page/plugin.php
+++ wikiplug/tools/toolsmng/handlers/page/plugin.php
@@ -20,7 +20,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 $plugins_root = dirname(__FILE__).'/../../../';
 $plugins = new plugins($plugins_root);
-$plugins->getPlugins(false);
+$plugins->getPlugins(true);
 $plugins_list = $plugins->getPluginsList();
 
 
