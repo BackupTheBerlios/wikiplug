9e35908cb73bc4d39b0d4651bde896d6099c89cf
mrflos <mrflos>
2008-09-18 12:35:25 +0000

    ajout droits ecriture

--- wikiplug/tools/templates/actions/barreredaction.php
+++ wikiplug/tools/templates/actions/barreredaction.php
@@ -3,51 +3,54 @@ if (!defined("WIKINI_VERSION"))
 {
         die ("acc&egrave;s direct interdit");
 }
-//barre de rédaction
+//barre de redaction
 
-//action pour le footer de wikini
-$wikini_barre_bas =
-'<div class="footer">'."\n";
-$wikini_barre_bas .=
-$this->FormOpen("", "RechercheTexte", "get")."\n";
-if ( $this->HasAccess("write") ) {
-	$wikini_barre_bas .= "<a href=\"".$this->href("edit")."\" title=\"Cliquez pour &eacute;diter cette page.\">&Eacute;diter cette page</a> ::\n";
-}
-if ( $this->GetPageTime() ) {
-	$wikini_barre_bas .= "<a href=\"".$this->href("revisions")."\" title=\"Cliquez pour voir les derni&egrave;res modifications sur cette page.\">".$this->GetPageTime()."</a> ::\n";
-}
-// if this page exists
-if ($this->page)
-{
-	// if owner is current user
-	if ($this->UserIsOwner())
-	{
-		$wikini_barre_bas .=
-		"Propri&eacute;taire&nbsp;: vous :: \n".
-		"<a href=\"".$this->href("acls")."\" title=\"Cliquez pour &eacute;diter les permissions de cette page.\">&Eacute;diter permissions</a> :: \n".
-		"<a href=\"".$this->href("deletepage")."\">Supprimer</a> :: \n";
+if ($this->HasAccess("write")) {
+	//action pour le footer de wikini
+	$wikini_barre_bas =
+	'<div class="footer">'."\n";
+	$wikini_barre_bas .=
+	$this->FormOpen("", "RechercheTexte", "get")."\n";
+	if ( $this->HasAccess("write") ) {
+		$wikini_barre_bas .= "<a href=\"".$this->href("edit")."\" title=\"Cliquez pour &eacute;diter cette page.\">&Eacute;diter cette page</a> ::\n";
 	}
-	else
+	if ( $this->GetPageTime() ) {
+		$wikini_barre_bas .= "<a href=\"".$this->href("revisions")."\" title=\"Cliquez pour voir les derni&egrave;res modifications sur cette page.\">".$this->GetPageTime()."</a> ::\n";
+	}
+	// if this page exists
+	if ($this->page)
 	{
-		if ($owner = $this->GetPageOwner())
+		// if owner is current user
+		if ($this->UserIsOwner())
 		{
-			$wikini_barre_bas .= "Propri&eacute;taire : ".$this->Format($owner);
+			$wikini_barre_bas .=
+			"Propri&eacute;taire&nbsp;: vous :: \n".
+			"<a href=\"".$this->href("acls")."\" title=\"Cliquez pour &eacute;diter les permissions de cette page.\">&Eacute;diter permissions</a> :: \n".
+			"<a href=\"".$this->href("deletepage")."\">Supprimer</a> :: \n";
 		}
 		else
 		{
-			$wikini_barre_bas .= "Pas de propri&eacute;taire ";
-			$wikini_barre_bas .= ($this->GetUser() ? "(<a href=\"".$this->href("claim")."\">Appropriation</a>)" : "");
+			if ($owner = $this->GetPageOwner())
+			{
+				$wikini_barre_bas .= "Propri&eacute;taire : ".$this->Format($owner);
+			}
+			else
+			{
+				$wikini_barre_bas .= "Pas de propri&eacute;taire ";
+				$wikini_barre_bas .= ($this->GetUser() ? "(<a href=\"".$this->href("claim")."\">Appropriation</a>)" : "");
+			}
+			$wikini_barre_bas .= " :: \n";
 		}
-		$wikini_barre_bas .= " :: \n";
 	}
+	$wikini_barre_bas .=
+	'<a href="'.$this->href("referrers").'" title="Cliquez pour voir les URLs faisant r&eacute;f&eacute;rence &agrave; cette page.">'."\n".
+	'R&eacute;f&eacute;rences</a> ::'."\n".
+	$this->Link($this->tag, "plugin", "Extensions")." ::\n".
+	'Recherche : <input name="phrase" size="15" class="searchbox" />'."\n".
+	$this->FormClose().
+	'</div>'."\n";
+	
+	echo $wikini_barre_bas;	
 }
-$wikini_barre_bas .=
-'<a href="'.$this->href("referrers").'" title="Cliquez pour voir les URLs faisant r&eacute;f&eacute;rence &agrave; cette page.">'."\n".
-'R&eacute;f&eacute;rences</a> ::'."\n".
-$this->Link($this->tag, "plugin", "Extensions")." ::\n".
-'Recherche : <input name="phrase" size="15" class="searchbox" />'."\n".
-$this->FormClose().
-'</div>'."\n";
 
-echo $wikini_barre_bas;
 ?>
--- wikiplug/tools/templates/actions/doubleclic.php
+++ wikiplug/tools/templates/actions/doubleclic.php
@@ -3,7 +3,7 @@ if (!defined("WIKINI_VERSION"))
 {
         die ("acc&egrave;s direct interdit");
 }
-if ($this->GetMethod() != 'edit') {
+if ( $this->GetMethod() != 'edit' && $this->HasAccess("write") ) {
 	//javascript du double clic (on peux passer en parametre une page wiki au editer en doublecliquant)  
 	$page = $this->GetParameter('page');
 	if (!empty($page)) {
