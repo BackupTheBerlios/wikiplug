e6163dbd8bef51f6cdc63b1d076c70dca194f6e3
mrflos <mrflos>
2009-07-24 15:01:36 +0000

    validité html

--- wikiplug/tools/templates/actions/parambody.php
+++ wikiplug/tools/templates/actions/parambody.php
@@ -4,7 +4,6 @@ if (!defined("WIKINI_VERSION"))
         die ("acc&egrave;s direct interdit");
 }
 //attributs du body
-$body_attr = ($message = $this->GetMessage()) ? "onLoad=\"alert('".addslashes($message)."');\" " : "";
-//$wikini_body = isset($_SESSION["message"])&&$_SESSION["message"]!='' ? "onLoad=\"alert('".$_SESSION["message"]."');\" " : "";
-echo $body_attr;
+$body_attr = ($message = $this->GetMessage()) ? "onload=\"alert('".addslashes($message)."');\" " : "";
+
 ?>
