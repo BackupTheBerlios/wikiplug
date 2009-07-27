1469800127f7285acec737a187a50011aa19ebe3
mrflos <mrflos>
2009-07-27 15:34:35 +0000

    validité html

--- wikiplug/tools/templates/actions/parambody.php
+++ wikiplug/tools/templates/actions/parambody.php
@@ -4,7 +4,7 @@ if (!defined("WIKINI_VERSION"))
         die ("acc&egrave;s direct interdit");
 }
 //attributs du body
-$body_attr = ($message = $this->GetMessage()) ? "onLoad=\"alert('".addslashes($message)."');\" " : "";
+$body_attr = ($message = $this->GetMessage()) ? "onload=\"alert('".addslashes($message)."');\" " : "";
 //$wikini_body = isset($_SESSION["message"])&&$_SESSION["message"]!='' ? "onLoad=\"alert('".$_SESSION["message"]."');\" " : "";
 echo $body_attr;
 ?>
