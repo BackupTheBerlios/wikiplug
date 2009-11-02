87a58d1b06f237f260c23d0462c74c98146c6e13
fbredow <fbredow>
2009-11-02 15:22:45 +0000

    Ajout de la gestion de la configuration dans wakka.config.php ou dans un fichier config dans le repertoire du plugin. Ouvrir pour le fichier config.php pour la doc.

--- wikiplug/tools/piwik/actions/header__.php
+++ wikiplug/tools/piwik/actions/header__.php
@@ -1,5 +1,10 @@
 <?php
 
+include("tools/clickheat/config.php");
+
+$piwik_server = "193.50.71.238";
+$piwik_path = "/piwik/";
+
 if (!defined("WIKINI_VERSION"))
 {
             die ("acc&egrave;s direct interdit");
@@ -20,7 +25,7 @@ if (version_compare(phpversion(), '5.0') < 0) {
 	<!-- Piwik -->
 	<!-- <a href="http://piwik.org" title="Website analytics" onclick="window.open(this.href);return(false);"> -->
 	<script type="text/javascript">
-	var pkBaseURL = (("https:" == document.location.protocol) ? "https://193.50.71.238/piwik/" : "http://193.50.71.238/piwik/");
+	var pkBaseURL = (("https:" == document.location.protocol) ? "https://'.$piwik_server.$piwik_path.'" : "http://'.$piwik_server.$piwik_path'");
 	document.write(unescape("%3Cscript src=\'" + pkBaseURL + "piwik.js\' type=\'text/javascript\'%3E%3C/script%3E"));
 	</script>
 	<script type="text/javascript">
@@ -31,7 +36,7 @@ if (version_compare(phpversion(), '5.0') < 0) {
 	piwik_log(piwik_action_name, piwik_idsite, piwik_url);
 	//-->
 	</script><object>
-	<noscript><p>Website analytics <img src="http://193.50.71.238/piwik/piwik.php" style="border:0" alt="piwik"/></p>
+	<noscript><p>Website analytics <img src="http://'.$piwik_server.$piwik_path.'piwik.php" style="border:0" alt="piwik"/></p>
 	</noscript></object></a>
 	<!-- /Piwik --> ',
 	$plugin_output_new);
