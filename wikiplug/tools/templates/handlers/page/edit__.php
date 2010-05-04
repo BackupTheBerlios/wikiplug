dd72b035f08e9fb20eae6dee7ab1ffebf972733c
mrflos <mrflos>
2010-05-04 09:50:47 +0000

    la configuration permet de cacher la sélection des templates par défaut

--- wikiplug/tools/templates/handlers/page/edit__.php
+++ wikiplug/tools/templates/handlers/page/edit__.php
@@ -14,15 +14,14 @@ $plugin_output_new = str_replace("<script type=\"text/javascript\">\n".
 				"document.getElementById(\"body\").onkeydown=fKeyDown;\n".
 				"</script>\n", '', $plugin_output_new);
 				
-if (!isset($this->config['hide_action_template']) or (isset($this->config['hide_action_template']) && !$this->config['hide_action_template'])) { // TODO : utiliser ACL
-
+if (!isset($this->config['hide_action_template']) or (isset($this->config['hide_action_template']) && !$this->config['hide_action_template'])) { 
+	$selecteur = '';
 	if ($this->HasAccess("write") && $this->HasAccess("read") && 
 	(!SEUL_ADMIN_ET_PROPRIO_CHANGENT_THEME || 
 	(SEUL_ADMIN_ET_PROPRIO_CHANGENT_THEME && ($this->UserIsAdmin() || $this->UserIsOwner())))) {
 		// Edition
-	
 		if (!isset($_POST["submit"]) or (isset($_POST["submit"]) && $_POST["submit"] != html_entity_decode('Aper&ccedil;u') && $_POST["submit"] != 'Sauver')) {
-			$selecteur = 'Th&egrave;me: <select name="theme" onchange="changeVal(this.value)">'."\n";
+			$selecteur .= 'Th&egrave;me: <select name="theme" onchange="changeVal(this.value)">'."\n";
 		    foreach(array_keys($this->config['templates']) as $key => $value) {
 		            if($value !== $this->config['favorite_theme']) {
 		                    $selecteur .= '<option value="'.$value.'">'.$value.'</option>'."\n";
@@ -56,35 +55,32 @@ if (!isset($this->config['hide_action_template']) or (isset($this->config['hide_
 		            }
 		    }
 		    $selecteur .= '</select>'."\n".'<br />'."\n";
-			//on ajoute la selection des styles
-			$plugin_output_new=preg_replace ('/\<input name=\"submit\" type=\"submit\" value=\"Sauver\"/',
-			$selecteur.'<input name="submit" type="submit" value="Sauver"', $plugin_output_new);
 			
 			//AJOUT DU JAVASCRIPT QUI PERMET DE CHANGER DYNAMIQUEMENT DE TEMPLATES			
-			$javascript = '<script type="text/javascript"><!--
+			$selecteur .= '<script type="text/javascript"><!--
 			var tab1 = new Array();
 			var tab2 = new Array();'."\n";
 			foreach(array_keys($this->config['templates']) as $key => $value) {
-		            $javascript .= '		tab1["'.$value.'"] = new Array(';
+		            $selecteur .= '		tab1["'.$value.'"] = new Array(';
 		            $nbocc=0;	           
 		            foreach($this->config['templates'][$value]["squelette"] as $key2 => $value2) {
-		            	if ($nbocc==0) $javascript .= '\''.$value2.'\'';
-		            	else $javascript .= ',\''.$value2.'\'';
+		            	if ($nbocc==0) $selecteur .= '\''.$value2.'\'';
+		            	else $selecteur .= ',\''.$value2.'\'';
 		            	$nbocc++;
 		            }
-		            $javascript .= ');'."\n";
+		            $selecteur .= ');'."\n";
 		            
-		            $javascript .= '		tab2["'.$value.'"] = new Array(';
+		            $selecteur .= '		tab2["'.$value.'"] = new Array(';
 		            $nbocc=0;
 		            foreach($this->config['templates'][$value]["style"] as $key3 => $value3) {
-		            	if ($nbocc==0) $javascript .= '\''.$value3.'\'';
-		            	else $javascript .= ',\''.$value3.'\'';
+		            	if ($nbocc==0) $selecteur .= '\''.$value3.'\'';
+		            	else $selecteur .= ',\''.$value3.'\'';
 		            	$nbocc++;
 		            }
-		            $javascript .= ');'."\n";	      
+		            $selecteur .= ');'."\n";	      
 		    }
 					
-			$javascript .= '		function changeVal(val){
+			$selecteur .= '		function changeVal(val){
 				
 				// pour vider la liste
 				document.ACEditor.squelette.options.length=0
@@ -103,11 +99,17 @@ if (!isset($this->config['hide_action_template']) or (isset($this->config['hide_
 				}					
 			}
 			//--></script>';
-			$plugin_output_new=preg_replace ('/\<input type="button" value="Annulation"/',
-			$javascript.'<input type="button" value="Annulation"',
-			$plugin_output_new);
 		}
+	} 
+	//quand le changement des valeurs du template est caché, il faut stocker les valeurs déja entrées pour ne pas retourner au template par défaut
+	elseif (SEUL_ADMIN_ET_PROPRIO_CHANGENT_THEME) {
+		$selecteur .= '<input type="hidden" name="theme" value="'.$this->config['favorite_theme'].'" />'."\n";
+		$selecteur .= '<input type="hidden" name="squelette" value="'.$this->config['favorite_squelette'].'" />'."\n";
+		$selecteur .= '<input type="hidden" name="style" value="'.$this->config['favorite_style'].'" />'."\n";
 	}
+	//on ajoute la selection des styles
+	$plugin_output_new=preg_replace ('/\<input name=\"submit\" type=\"submit\" value=\"Sauver\"/',
+	$selecteur.'<input name="submit" type="submit" value="Sauver"', $plugin_output_new);
 }
 
 ?>
--- wikiplug/tools/templates/wiki.php
+++ wikiplug/tools/templates/wiki.php
@@ -10,19 +10,19 @@ if (!defined("WIKINI_VERSION"))
 // Dans Wakka.config.php, on peut preciser : favorite_theme, favorite_style, favorite_squelette,  hide_action_template 
 // Sinon, on prend les parametres ci dessous :
 
-// Configuration du fonctionnement des templates : faut il laisser le choix autre que par dï¿½faut 
+// Configuration du fonctionnement des templates : faut il laisser le choix autre que par défaut 
 define('FORCER_TEMPLATE_PAR_DEFAUT', (isset($wakkaConfig['hide_action_template'])) ? $wakkaConfig['hide_action_template'] : false);
 
-//Theme par dï¿½faut
+//Theme par défaut
 define ('THEME_PAR_DEFAUT', (isset($wakkaConfig['favorite_theme'])) ? $wakkaConfig['favorite_theme'] : 'generique');
 
-//Style par dï¿½faut
+//Style par défaut
 define ('CSS_PAR_DEFAUT', (isset($wakkaConfig['favorite_style'])) ? $wakkaConfig['favorite_style'] : 'marron.css');
 
-//squelette par dï¿½faut
+//squelette par défaut
 define ('SQUELETTE_PAR_DEFAUT', (isset($wakkaConfig['favorite_squelette'])) ? $wakkaConfig['favorite_squelette'] : '2colonnesgauche.tpl.html');
 
-//pour que seul le propriï¿½taire et l'admin puissent changer de theme
+//pour que seul le propriétaire et l'admin puissent changer de theme
 define ('SEUL_ADMIN_ET_PROPRIO_CHANGENT_THEME', true);
 
 // Desactivation de l'extension template si l'extension navigation est presente et active. 
@@ -31,6 +31,7 @@ if (isset($plugins_list['navigation']))
 	unset($k);	
 	return;
 }
+
 
 //on cherche tous les dossiers du repertoire themes et des sous dossier styles et squelettes, et on les range dans le tableau $wakkaConfig['templates']
 $repertoire = 'tools'.DIRECTORY_SEPARATOR.'templates'.DIRECTORY_SEPARATOR.'themes';
@@ -54,6 +55,7 @@ while (false !== ($file = readdir($dir))) {
 }
 closedir($dir);
 if (is_array($wakkaConfig)) ksort($wakkaConfig['templates']);
+
 
 //si POST
 //=======Changer de theme=================================================================================================
@@ -138,7 +140,7 @@ else
 }
 
 
-//on rï¿½cupï¿½re les valeurs du template associï¿½es ï¿½ la page
+//on récupére les valeurs du template associées à la page
 if (!$wakkaConfig['hide_action_template'] && $act=preg_match_all ("/".'(\\{\\{template)'.'(.*?)'.'(\\}\\})'."/is", $contenu["body"], $matches)) {
      $i = 0; $j = 0;
      foreach($matches as $valeur) {
