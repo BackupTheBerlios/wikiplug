38d930ce1aabe1313b2fb6a8e500697c681e3c4b
ddelon <ddelon>
2010-10-05 09:52:04 +0000

    Gestion administrateur

--- wikiplug/tools/templates/actions/barreredaction.php
+++ wikiplug/tools/templates/actions/barreredaction.php
@@ -16,30 +16,39 @@ if ($this->HasAccess("write")) {
 		$wikini_barre_bas .= "<a href=\"".$this->href("revisions")."\" title=\"Cliquez pour voir les derni&egrave;res modifications sur cette page.\">".$this->GetPageTime()."</a> ::\n";
 	}
 	// if this page exists
+
 	if ($this->page)
-	{
-		// if owner is current user
-		if (($this->UserIsOwner()) || ($this->UserIsAdmin()))
-		{
-			$wikini_barre_bas .=
-			"Propri&eacute;taire&nbsp;: vous :: \n".
-			"<a href=\"".$this->href("acls")."\" title=\"Cliquez pour &eacute;diter les permissions de cette page.\">Permissions</a> :: \n".
-			"<a href=\"".$this->href("deletepage")."\">Supprimer</a> :: \n";
-		}
-		else
-		{
-			if ($owner = $this->GetPageOwner())
-			{
-				$wikini_barre_bas .= "Propri&eacute;taire : $owner";
-			}
-			else
-			{
-				$wikini_barre_bas .= "Pas de propri&eacute;taire ";
-				$wikini_barre_bas .= ($this->GetUser() ? "(<a href=\"".$this->href("claim")."\">Appropriation</a>)" : "");
-			}
-			$wikini_barre_bas .= " :: \n";
-		}
-	}
+    {   
+                // if owner is current user
+                if ($this->UserIsOwner() )   
+                {   
+                        $wikini_barre_bas .=
+                        "Propri&eacute;taire&nbsp;: vous :: \n";
+                }
+                else
+                {   
+                        if ($owner = $this->GetPageOwner())
+                        {
+                                $wikini_barre_bas .= "Propri&eacute;taire : ".$this->Format($owner);
+                        }   
+                        else
+                        {   
+                                $wikini_barre_bas .= "Pas de propri&eacute;taire ";
+                                $wikini_barre_bas .= ($this->GetUser() ? "(<a href=\"".$this->href("claim")."\">Appropriation</a>)" : "");
+                        }
+                        $wikini_barre_bas .= " :: \n";
+                }
+
+                if ($this->UserIsOwner() || $this->UserIsAdmin()) { 
+                        $wikini_barre_bas .=
+                        "<a href=\"".$this->href("acls")."\" title=\"Cliquez pour &eacute;diter les permissions de cette page.\">&Eacute;diter permissions</a> :: \n".
+                        "<a href=\"".$this->href("deletepage")."\">Supprimer</a> :: \n";
+                }   
+
+      }   
+	
+	
+	
 	$wikini_barre_bas .=
 	'<a href="'.$this->href("referrers").'" title="Cliquez pour voir les URLs faisant r&eacute;f&eacute;rence &agrave; cette page.">'."\n".
 	'R&eacute;f&eacute;rences</a> :: '."\n".
